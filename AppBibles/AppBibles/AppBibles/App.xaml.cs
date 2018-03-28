95cada46927cbe91a146f3450c9e5b8e050f701c
Juan <Jocampoa@intergrupo.com>
2018-03-28 16:12:54 -0500

    Login View Model

---
+++ AppBibles/AppBibles/AppBibles/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace AppBibles
+{
+    using Xamarin.Forms;
+    using AppBibles.Views;
+
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
